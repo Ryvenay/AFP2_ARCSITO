04699b80757e98c4b6ba8080a6880e8b95477594
Dániel Árvai <arvai96@gmail.com>
2022-03-29 16:00:45 +0200

    feat(rest_api): added database configuration

---
+++ src/backend/database.php
@@ -0,0 +1,40 @@
+<?php
+
+define('DB_TYPE','mysql');
+define('DB_HOST', $_ENV['MYSQL_HOST']);
+define('DB_NAME', $_ENV['MYSQL_DATABASE']);
+define('DB_USER', $_ENV['MYSQL_USER']);
+define('DB_PASS', $_ENV['MYSQL_PASSWORD']);
+define('DB_PORT', $_ENV['MYSQL_PORT']);
+
+class db {
+
+
+    function getConnection() {
+        $dsn = DB_TYPE.':host='.DB_HOST.':'.DB_PORT.';dbname='.DB_NAME;
+        $connection = new PDO($dsn,DB_USER, DB_PASS);
+        $connection->exec("SET NAMES 'utf8'");
+        return $connection;
+    }
+
+    public function execute($query, $params) {
+        $connection = $this->getConnection();
+        $stmt = $connection->prepare($query);
+        $success = $stmt->execute($params);
+        $stmt->closeCursor();
+        $connection = null;
+        return $success;
+    }
+
+    public function query($query, $params = []) {
+        $connection = $this->getConnection();
+        $stmt = $connection->prepare($query);
+        $success = $stmt->execute($params);
+
+        $records = $success ? $stmt->fetchAll(PDO::FETCH_ASSOC) : [];
+        $stmt->closeCursor();
+        $connection = null;
+
+        return $records;
+    }
+}
---
+++ src/backend/db_init/database.sql
@@ -0,0 +1,58 @@
+use sport_events;
+
+CREATE TABLE `Event` (
+  `id` int PRIMARY KEY,
+  `sport_id` int,
+  `name` varchar(255) NOT NULL,
+  `date` datetime NOT NULL
+);
+
+CREATE TABLE `Participant` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` char(60) NOT NULL,
+  `neptun_code` char(6),
+  `department_id` int,
+  `city` varchar(255),
+  `type` int
+);
+
+CREATE TABLE `ParticipantType` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255)
+);
+
+CREATE TABLE `Sport` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255)
+);
+
+CREATE TABLE `Event_Participant` (
+  `event_id` int,
+  `participant_id` int,
+  PRIMARY KEY (`event_id`, `participant_id`)
+);
+
+CREATE TABLE `Department` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255) NOT NULL
+);
+
+CREATE TABLE `Administrator` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` char(60) NOT NULL
+);
+
+ALTER TABLE `Participant` ADD FOREIGN KEY (`department_id`) REFERENCES `Department` (`id`);
+
+ALTER TABLE `Participant` ADD FOREIGN KEY (`type`) REFERENCES `ParticipantType` (`id`);
+
+ALTER TABLE `Event` ADD FOREIGN KEY (`sport_id`) REFERENCES `Sport` (`id`);
+
+ALTER TABLE `Event_Participant` ADD FOREIGN KEY (`event_id`) REFERENCES `Event` (`id`);
+
+ALTER TABLE `Event_Participant` ADD FOREIGN KEY (`participant_id`) REFERENCES `Participant` (`id`);
+
