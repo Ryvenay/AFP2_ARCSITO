881f9b6bd2a2cbc7876e8811ac3fafb1f14d5325
Árvai Dániel <arvai96@gmail.com>
2022-05-03 17:23:58 +0200

    feat(admin_site): added footer

--- src/frontend/admin_site/src/components/Footer.js
+++ src/frontend/admin_site/src/components/Footer.js
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom';
+
 function Footer() {
     return (
-        <div>
-            Készítette: arcsito &copy; 2022
+        <div className="container">
+            <footer className="py-3 my-4">
+                <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link px-2 text-muted">Kezdőlap</Link>
+                    </li>
+                    <li className="nav-item">
+                        <a href="https://github.com/Ryvenay/AFP2_ARCSITO" target="_blank" rel="noreferrer" className="nav-link px-2 text-muted">GitHub</a>
+                    </li>
+                </ul>
+                <p className="text-center text-muted">&copy; 2022 arcsito</p>
+            </footer>
         </div>
     )
 }
